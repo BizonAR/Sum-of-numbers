c4277d7bd8ac6d84d03eb3460cae737178d9069d
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 09:54:51 +0500

    Sum of numbers

---
+++ SumOfNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNumbers
+{
+	internal class SumOfNumbers
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+			int maxValue = 100;
+			int divisor1 = 3;
+			int divisor2 = 5;
+			int number = random.Next(maxValue + 1);
+			int sum = 0;
+			int startNumber = 3;
+
+			Console.WriteLine(number);
+
+			for (int i = startNumber; i <= number; ++i)
+			{
+				if (i % divisor1 == 0 || i % divisor2 == 0)
+					sum += i;
+			}
+
+			Console.WriteLine(sum);
+		}
+	}
+}
